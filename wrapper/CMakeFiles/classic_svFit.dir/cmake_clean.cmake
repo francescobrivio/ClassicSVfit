40b72bbf635bfca66b13c4f8d199eb3019a511ef
oponcet <oceane.poncet@cern.ch>
2024-06-05 14:51:09 +0200

    remove files

--- wrapper/CMakeFiles/classic_svFit.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/classic_svFit.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- wrapper/CMakeFiles/pybind_wrapper.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pybind_wrapper.
-# This may be replaced when dependencies are built.
